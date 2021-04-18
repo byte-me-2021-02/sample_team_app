106755ecf283a545bf224f35274506803ec47187
Mat <mvello66@gmail.com>
2021-04-18 17:44:53 -0400

    adds message to the index of appropriate_controller_for_things_controller

--- app/controllers/api/appropriate_controller_for_things_controller.rb
+++ app/controllers/api/appropriate_controller_for_things_controller.rb
@@ -1,2 +1,5 @@
 class Api::AppropriateControllerForThingsController < ApplicationController
+  def index
+    render "index.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/pages" => "pages#index"
+    get "/appropriate_controller_for_things_controller" => "appropriate_controller_for_things_controller#index"
   end
 end
